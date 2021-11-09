ecd98f74997874117a36be72308fb7a473d8d4bb
Matthew McKinnon <mmckinnon@comprofix.com>
2021-11-09 13:24:55 +1000

    Added requirements

--- main.py
+++ main.py
@@ -1,28 +1,24 @@
-import discord
+# bot.py
 import os
+import discord
 from dotenv import load_dotenv
 
 load_dotenv()
+TOKEN = os.getenv('TOKEN')
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.members = True
+
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if (message.content.startswith('$hello')):
-        await message.channel.send('Hello!')
+    print(f'{client.user.name} has connected to Discord!')
 
 @client.event
 async def on_member_join(member):
-        default_role = discord.utils.get(member.guild.roles, id=907235072682389554)
-        message.user.send('Welcome!')
-        await member.add_roles(default_role)
+    print(f"{member} has joined the server.")
+    ment = member.mention
+    await member.send(f"Welcome! {ment}")
 
-
-client.run(os.getenv('TOKEN'))
+client.run(TOKEN)
